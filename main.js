aa11403813f678259912921e07dde99c16b61d2c
Jun <email2mrjunlee@gmail.com>
2021-02-22 22:34:53 +1300

    inhanced the performace by changing style.css

--- main.js
+++ main.js
@@ -2,17 +2,30 @@ const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
+window.addEventListener('load', () => {
+  document.addEventListener('mousemove', event => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(targetHalfHeight);
 
-document.addEventListener('mousemove', event => {
-  const x = event.clientX;
-  const y = event.clientY;
+    vertical.style.transform = `translateX(${x}px)`
+    horizontal.style.transform = `translateY(${y}px)`
+    tag.style.transform = `translate(${x}px,${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px,${y - targetHalfHeight}px)`;
+    tag.innerHTML = `${x},${y}`
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.innerHTML = `${x},${y}`
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    // tag.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+    // tag.innerHTML = `${x},${y}`
+    // target.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+  });
+
 });
+
